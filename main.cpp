8525daa421a39269d539b22d9f6c97fdae0dafc6
tkbit <jeremiahmrosa@gmail.com>
2022-01-22 22:42:16 -0800

    clean up code

--- main.cpp
+++ main.cpp
@@ -3,35 +3,33 @@
 #include<SDL.h>
 
   SDL_Window* window;
-  Uint32 windowID;
-  int statues = 0;
-  bool programRun = true;
+  Uint32 windowID = NULL;
+  int status = 0;
+  bool programRunning = true;
 
 void initSDL() {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-        statues = 1;
-        programRun = false;
+        status = 1;
+        programRunning = false;
     }
-
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
 
     // Create an application window with the following settings:
     window = SDL_CreateWindow(
-        "FAT MAN",                  // window title
+        "Fatman",                  // window title
         SDL_WINDOWPOS_UNDEFINED,           // initial x position
         SDL_WINDOWPOS_UNDEFINED,           // initial y position
         640,                               // width, in pixels
         480,                               // height, in pixels
         SDL_WINDOW_OPENGL                  // flags - see below
     );
-    Uint32 windowID = SDL_GetWindowID(window);
-    // Check that the window was successfully created
     if (window == NULL) {
-        // In the case that the window could not be made...
         printf("Could not create window: %s\n", SDL_GetError());
-        statues = 1;
-        programRun = false;
+        status = 1;
+        programRunning = false;
+    }
+    else {
+        windowID = SDL_GetWindowID(window);
     }
 }
 
@@ -54,17 +52,18 @@ void checkInput() {
             break;
         }
         case SDL_QUIT: {
-            statues = 0;
-            programRun = false;
+            status = 0;
+            programRunning = false;
         }
         }
     }
 }
 
-int main(int argc , char* argv[]){
+int main(int argc, char* argv[]){
+
     initSDL();
 
-    while (programRun) {
+    while (programRunning) {
         checkInput();
         SDL_Delay(1);
     }
@@ -74,5 +73,5 @@ int main(int argc , char* argv[]){
 
     SDL_Quit();
 
-    return statues;
+    return status;
 }
