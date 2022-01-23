6ca53c4bc27e5ebef9e5ca280bdbb52d0b658632
MicahO365 <m.ober365@gmail.com>
2022-01-22 22:27:39 -0800

    organize

--- main.cpp
+++ main.cpp
@@ -4,14 +4,15 @@
 
   SDL_Window* window;
   Uint32 windowID;
+  int statues = 0;
+  bool programRun = true;
 
-int initSDL() {
+void initSDL() {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-        return 1;
+        statues = 1;
+        programRun = false;
     }
-
-                      // Declare a pointer
 
     SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
 
@@ -29,36 +30,42 @@ int initSDL() {
     if (window == NULL) {
         // In the case that the window could not be made...
         printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
+        statues = 1;
+        programRun = false;
+    }
+}
+
+void checkInput() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+
+        case SDL_WINDOWEVENT: {
+            if (event.window.windowID == windowID) {
+                switch (event.window.event) {
+
+                case SDL_WINDOWEVENT_CLOSE: {
+                    event.type = SDL_QUIT;
+                    SDL_PushEvent(&event);
+                    break;
+                }
+                }
+            }
+            break;
+        }
+        case SDL_QUIT: {
+            statues = 0;
+            programRun = false;
+        }
+        }
     }
 }
 
 int main(int argc , char* argv[]){
     initSDL();
 
-    while (1) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            switch (event.type) {
-
-            case SDL_WINDOWEVENT: {
-                if (event.window.windowID == windowID) {
-                    switch (event.window.event) {
-
-                    case SDL_WINDOWEVENT_CLOSE: {
-                        event.type = SDL_QUIT;
-                        SDL_PushEvent(&event);
-                        break;
-                    }
-                    }
-                }
-                break;
-            }
-            case SDL_QUIT: {
-                return 0;
-            }
-            }
-        }
+    while (programRun) {
+        checkInput();
         SDL_Delay(1);
     }
 
@@ -67,5 +74,5 @@ int main(int argc , char* argv[]){
 
     SDL_Quit();
 
-    return 0;
+    return statues;
 }
